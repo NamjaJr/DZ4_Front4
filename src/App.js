006c45da69072771d20468dbf8657d4648283d1f
NamjaJr <hanruslan144@gmail.com>
2024-05-21 21:14:29 +0600

    dz4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from "./pages/MainPage/MainPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import {useEffect, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [value,setValue] = useState({name:'',lastname:''})
+  useEffect(() => {
+    const userName = prompt('Введите ваше имя')
+    const userLastname = prompt('Введите вашу фамилию')
+    if (userName !== null && userLastname !== null){
+      setValue({name: userName,lastname: userLastname})
+    }
+  }, []);
+
+
+  if (value.name === 'John' && value.lastname === 'Johns'){
+    return(
+        <MainPage user={value}/>
+    )
+  }else {
+    return(
+        <ErrorPage user={value}/>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage({user}) {
+    return (
+        <div>
+            <p>Добро пожаловать {user.name} {user.lastname}, мы тебя ждали</p>
+        </div>
+    );
+}
+
+export default MainPage;
